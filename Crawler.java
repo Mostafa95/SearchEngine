2044f5307ea33a74c7f8b6ec27b6fe9067ee6830
Mostafa <mostafaelmohandes95@gmail.com>
2017-03-27 22:33:02 +0200

    Error loading same page

--- Crawler.java
+++ Crawler.java
@@ -1,4 +1,3 @@
-package Package;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -25,152 +24,117 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Crawler extends Thread {
-	public static DB db = new DB();
-	public static HashMap Vis_URL = new HashMap();
-	public static int thrs;
-	public int maxdepth = 2;
 
-	public int cnt = 0;
-	public int states[];
-	public int statecnt = 0;
-	public Number statenum = 0;
-	public int cnter = 0;
-	/** Queue to store URL's, can be accessed by multiple threads **/
-	public ConcurrentLinkedQueue<String> tovisit = new ConcurrentLinkedQueue<String>();
+    public static HashMap Vis_URL = new HashMap();
+    public int maxdepth = 4;
+    public static int VisUrlSz = 0;
+    public int cnt = 0;
+    public Number statenum = 0;
 
-	/** ArrayList of visited URL's **/
-	public ArrayList<String> visited = new ArrayList<String>();
+    public void processPage(String URL, int depth) {
 
-	public void processPage(String URL, int depth) throws SQLException, IOException {
-		// check if the given URL is already in database
-		// cnt++;
+        if (depth == maxdepth || VisUrlSz == 50) {
+            return;
+        }
 
-		if (depth == maxdepth)
-			return;
+        if (URL.endsWith("/")) {
+            URL = URL.substring(0, URL.length() - 1);
+        }
 
-		String Suburl, sql;
-		if (URL.endsWith("/")) {
-			Suburl = URL.substring(0, URL.length() - 1);
-		}
-		URL = Suburl;
-		Object Val = Vis_URL.get(URL);
-		if (Val == null) {
-			Vis_URL.put(URL, 0);
-		}
-		if ((int) Val == 0) { // URL found but not parsed
-			Vis_URL.replace(URL, 0, 1);
-			Document doc = Jsoup.connect(URL).get();
+        Object Val = Vis_URL.get(URL);
+        if (Val == null) {
+            Vis_URL.put(URL, 0);
+        }
 
-			try (BufferedWriter file = new BufferedWriter(
-					new FileWriter("C:\\Ahmed\\uni\\APT\\" + cnt + ".html", false))) {
-				file.write((doc.toString()));
-			} catch (IOException e) {
-				System.out.println(e);
-			}
-			// get all links and recursively call the processPage method
-			String sqlid = "select RecordID from Record where URL = '" + URL + "'";
-			ResultSet rss = db.runSql(sqlid);
-			if (rss.next()) {
-				statenum = ((Number) rss.getObject("RecordID")).intValue();
-				System.out.println(statenum);
-			}
+        // URL found but not parsed
+        Vis_URL.replace(URL, 0, 1);
+        VisUrlSz++;
+        Document doc = null;
+        try {
+            System.out.println("D5l be "+URL);
+            doc = Jsoup.connect(URL).get();
+        } catch (IOException e) {
+            System.out.println("Doc Not connected to URL");
+        }
+        System.out.println("3dda");
+        // HTML file
+        try (BufferedWriter file = new BufferedWriter(
+                new FileWriter("/home/mostafa/NetBeansProjects/Indexer/HTMLs/" + cnt++ + ".html", false))) {
+            file.write((doc.toString()));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        // State file
+//        try (BufferedWriter statefile = new BufferedWriter(
+//                new FileWriter("C:\\Ahmed\\uni\\APT\\State.txt", false))) {
+//            statefile.write((int) statenum); //// HAAAAA<<<
+//        } catch (IOException e) {
+//            System.out.println(e);
+//        }
 
-			try (BufferedWriter statefile = new BufferedWriter(
-					new FileWriter("C:\\Ahmed\\uni\\APT\\State.txt", false))) {
-				statefile.write((int) statenum); //// HAAAAA<<<
-			} catch (IOException e) {
-				System.out.println(e);
-			}
+        Elements links = doc.body().getElementsByAttribute("href");
+        for (Element link : links) {
+            Vis_URL.put(link.attr("abs:href"), 0);
 
-			Elements links = doc.select("a[href]");
-			for (Element link : links) {
-				Vis_URL.put(link.toString(), 0);
-				// Vis_URL.
-			}
+        }
 
-			// for(int i=0; i<Vis_URL.size();i++){
-			Iterator it = Vis_URL.entrySet().iterator();
-			while (it.hasNext()) {
-				Map.Entry pair = (Map.Entry) it.next();
-				if ((int) pair.getValue() == 0)
-					processPage(pair.getKey().toString(), ++depth);
+        Iterator it = Vis_URL.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            if ((int) pair.getValue() == 0) {
+                processPage(pair.getKey().toString(), ++depth);
+            }
 
-				// System.out.println(pair.getKey() + " = " +
-				// pair.getValue());
-				it.remove(); // avoids a ConcurrentModificationException
-			}
+            it.remove(); // avoids a ConcurrentModificationException
+        }
 
-			// hhhhggggf
+//        try (BufferedWriter statefile = new BufferedWriter(
+//                new FileWriter("C:\\Ahmed\\uni\\APT\\State.txt", false))) {
+//            statefile.write((int) statenum); //// HAAAAA<<<
+//        } catch (IOException e) {
+//            System.out.println(e);
+//        }
+    }
 
-			// ResultSet rs = db.runSql(sql);
+    public void mainprocessPage(String URL) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(URL).get();
+        } catch (IOException e) {
+            System.out.println("Doc Not connected to URL");
+        }
 
-			try (BufferedWriter statefile = new BufferedWriter(
-					new FileWriter("C:\\Ahmed\\uni\\APT\\State.txt", false))) {
-				statefile.write((int) statenum); //// HAAAAA<<<
-			} catch (IOException e) {
-				System.out.println(e);
-			}
-			// Document doc = Jsoup.connect(URL).get();
+        try (BufferedWriter file = new BufferedWriter(new FileWriter("/home/mostafa/NetBeansProjects/Indexer/HTMLs/" + cnt++ + ".html", false))) {
+            file.write((doc.toString()));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        // get all links and recursively call the processPage method
+      // Elements Links = doc.getElementsByTag("body");
+       Elements temp = doc.body().getElementsByAttribute("href");
+//       String temp = Links.attr("href");
+//       String [] str = temp.split(" ");
+//       System.out.println(temp);
+       
+        for (Element e : temp) {
+          // System.out.println(e.attr("href"));
+            Vis_URL.put(e.attr("abs:href"), 0);
+        }
 
-			// }
-		}
-	}
+    }
 
-	// }
-	// cnter++;
-	// if (cnter < thrs + 1) {
-	// Crawler crawl = new Crawler();
-	// crawl.processPage(link.attr("abs:href"));
-	// Thread tt = new Thread(crawl);
-	// tt.start();
-	// } // depth++;
-	// else
-	// processPage(link.attr("abs:href"));
-	// // String theLink = htmls.attr("abs:href");
-	// // if(theLink.startsWith("http") &&
-	// // !(tovisit.contains((Object)theLink)))
-	// // {
-	// // tovisit.add(theLink);
-	// // visited.add(theLink);
-	// // System.out.println(theLink);
-	// // }
-	// }
-	// }else{
-
-	// }
-
-	public void mainprocessPage(String URL) throws SQLException, IOException {
-		// check if the given URL is already in database
-		// cnt++;
-		// String sql = "INSERT INTO `Crawler`.`Record` " + "(`URL`) VALUES " +
-		// "(?);";
-		// PreparedStatement stmt = db.conn.prepareStatement(sql,
-		// Statement.RETURN_GENERATED_KEYS);
-		// stmt.setString(1, URL);
-		// stmt.execute();
-		// // get useful information
-		Document doc = Jsoup.connect(URL).get();
-
-		try (BufferedWriter file = new BufferedWriter(new FileWriter("C:\\Ahmed\\uni\\APT\\" + cnt + ".html", false))) {
-			file.write((doc.toString()));
-		} catch (IOException e) {
-			System.out.println(e);
-		}
-		// get all links and recursively call the processPage method
-		Elements links = doc.select("a[href]");
-
-		for (Element e : links) {
-			Vis_URL.put(e.toString(), 0);
-		}
-
-	}
-
-	@Override
-	public void run() {
-		String sq = "select * from Record where URL = '" + URL + "'";
-		ResultSet rs = db.runSql(sql);
-		if (rs.next()) {
-		// TODO Auto-generated method stub
-
-	}
+    @Override
+    public void run() {
+        Iterator it = Vis_URL.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            if ((int) pair.getValue() == 0) {
+                System.out.println(pair.getKey().toString());
+                processPage(pair.getKey().toString(), 1);
+                break;
+            }
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+    }
 }
+
